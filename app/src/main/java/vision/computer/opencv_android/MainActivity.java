3100e81ad1fd22bb39b84ead9f4d691c022081f5
Manuel <mlagunas94@gmail.com>
2016-02-17 23:52:46 +0100

    Camera taking photos

--- app/src/main/java/vision/computer/opencv_android/MainActivity.java
+++ app/src/main/java/vision/computer/opencv_android/MainActivity.java
@@ -1,97 +1,374 @@
 package vision.computer.opencv_android;
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Mat;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import java.io.File;
+import java.util.List;
 
-import android.app.Activity;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.Size;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.SurfaceView;
+import android.view.SubMenu;
+import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements CvCameraViewListener2 {
-    private static final String TAG = "OCVSample::Activity";
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
-    private CameraBridgeViewBase mOpenCvCameraView;
-    private boolean              mIsJavaCamera = true;
-    private MenuItem             mItemSwitchCamera = null;
+// Use the deprecated Camera class.
+@SuppressWarnings("deprecation")
 
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
-                    Log.i(TAG, "OpenCV loaded successfully");
-                    mOpenCvCameraView.enableView();
-                } break;
-                default:
-                {
-                    super.onManagerConnected(status);
-                } break;
+public class MainActivity extends ActionBarActivity
+        implements CvCameraViewListener2 {
+    // A tag for log output.
+    private static final String TAG =
+            MainActivity.class.getSimpleName();
+
+    // A key for storing the index of the active camera.
+    private static final String STATE_CAMERA_INDEX = "cameraIndex";
+
+    // A key for storing the index of the active image size.
+    private static final String STATE_IMAGE_SIZE_INDEX =
+            "imageSizeIndex";
+
+    // An ID for items in the image size submenu.
+    private static final int MENU_GROUP_ID_SIZE = 2;
+
+    // The index of the active camera.
+    private int mCameraIndex;
+
+    // The index of the active image size.
+    private int mImageSizeIndex;
+
+    // Whether the active camera is front-facing.
+    // If so, the camera view should be mirrored.
+    private boolean mIsCameraFrontFacing;
+
+    // The number of cameras on the device.
+    private int mNumCameras;
+
+    // The image sizes supported by the active camera.
+    private List<Size> mSupportedImageSizes;
+
+    // The camera view.
+    private CameraBridgeViewBase mCameraView;
+
+    // Whether the next camera frame should be saved as a photo.
+    private boolean mIsPhotoPending;
+
+    // A matrix that is used when saving photos.
+    private Mat mBgr;
+
+    // Whether an asynchronous menu action is in progress.
+    // If so, menu interaction should be disabled.
+    private boolean mIsMenuLocked;
+
+    // The OpenCV loader callback.
+    private BaseLoaderCallback mLoaderCallback =
+            new BaseLoaderCallback(this) {
+                @Override
+                public void onManagerConnected(final int status) {
+                    switch (status) {
+                        case LoaderCallbackInterface.SUCCESS:
+                            Log.d(TAG, "OpenCV loaded successfully");
+                            mCameraView.enableView();
+                            //mCameraView.enableFpsMeter();
+                            mBgr = new Mat();
+                            break;
+                        default:
+                            super.onManagerConnected(status);
+                            break;
+                    }
+                }
+            };
+
+    // Suppress backward incompatibility errors because we provide
+    // backward-compatible fallbacks.
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final Window window = getWindow();
+        window.addFlags(
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        if (savedInstanceState != null) {
+            mCameraIndex = savedInstanceState.getInt(
+                    STATE_CAMERA_INDEX, 0);
+            mImageSizeIndex = savedInstanceState.getInt(
+                    STATE_IMAGE_SIZE_INDEX, 0);
+        } else {
+            mCameraIndex = 0;
+            mImageSizeIndex = 0;
+        }
+
+        final Camera camera;
+        if (Build.VERSION.SDK_INT >=
+                Build.VERSION_CODES.GINGERBREAD) {
+            CameraInfo cameraInfo = new CameraInfo();
+            Camera.getCameraInfo(mCameraIndex, cameraInfo);
+            mIsCameraFrontFacing =
+                    (cameraInfo.facing ==
+                            CameraInfo.CAMERA_FACING_FRONT);
+            mNumCameras = Camera.getNumberOfCameras();
+            camera = Camera.open(mCameraIndex);
+        } else { // pre-Gingerbread
+            // Assume there is only 1 camera and it is rear-facing.
+            mIsCameraFrontFacing = false;
+            mNumCameras = 1;
+            camera = Camera.open();
+        }
+        final Parameters parameters = camera.getParameters();
+        camera.release();
+        mSupportedImageSizes =
+                parameters.getSupportedPreviewSizes();
+        final Size size = mSupportedImageSizes.get(mImageSizeIndex);
+
+        mCameraView = new JavaCameraView(this, mCameraIndex);
+        mCameraView.setMaxFrameSize(size.width, size.height);
+        mCameraView.setCvCameraViewListener(this);
+        setContentView(mCameraView);
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the current camera index.
+        savedInstanceState.putInt(STATE_CAMERA_INDEX, mCameraIndex);
+
+        // Save the current image size index.
+        savedInstanceState.putInt(STATE_IMAGE_SIZE_INDEX,
+                mImageSizeIndex);
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    // Suppress backward incompatibility errors because we provide
+    // backward-compatible fallbacks.
+    @SuppressLint("NewApi")
+    @Override
+    public void recreate() {
+        if (Build.VERSION.SDK_INT >=
+                Build.VERSION_CODES.HONEYCOMB) {
+            super.recreate();
+        } else {
+            finish();
+            startActivity(getIntent());
+        }
+    }
+
+    @Override
+    public void onPause() {
+        if (mCameraView != null) {
+            mCameraView.disableView();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0,
+                this, mLoaderCallback);
+        mIsMenuLocked = false;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mCameraView != null) {
+            mCameraView.disableView();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        if (mNumCameras < 2) {
+            // Remove the option to switch cameras, since there is
+            // only 1.
+            menu.removeItem(R.id.menu_next_camera);
+        }
+        int numSupportedImageSizes = mSupportedImageSizes.size();
+        if (numSupportedImageSizes > 1) {
+            final SubMenu sizeSubMenu = menu.addSubMenu(
+                    R.string.menu_image_size);
+            for (int i = 0; i < numSupportedImageSizes; i++) {
+                final Size size = mSupportedImageSizes.get(i);
+                sizeSubMenu.add(MENU_GROUP_ID_SIZE, i, Menu.NONE,
+                        String.format("%dx%d", size.width,
+                                size.height));
             }
         }
-    };
-
-    public MainActivity() {
-        Log.i(TAG, "Instantiated new " + this.getClass());
+        return true;
     }
 
-    /** Called when the activity is first created. */
+    // Suppress backward incompatibility errors because we provide
+    // backward-compatible fallbacks (for recreate).
+    @SuppressLint("NewApi")
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        Log.i(TAG, "called onCreate");
-        super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if (mIsMenuLocked) {
+            return true;
+        }
+        if (item.getGroupId() == MENU_GROUP_ID_SIZE) {
+            mImageSizeIndex = item.getItemId();
+            recreate();
 
-        setContentView(R.layout.activity_main);
+            return true;
+        }
+        switch (item.getItemId()) {
+            case R.id.menu_next_camera:
+                mIsMenuLocked = true;
 
-        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.java_surface_view);
+                // With another camera index, recreate the activity.
+                mCameraIndex++;
+                if (mCameraIndex == mNumCameras) {
+                    mCameraIndex = 0;
+                }
+                mImageSizeIndex = 0;
+                recreate();
 
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+                return true;
+            case R.id.menu_take_photo:
+                mIsMenuLocked = true;
 
-        mOpenCvCameraView.setCvCameraViewListener(this);
-    }
+                // Next frame, take the photo.
+                mIsPhotoPending = true;
 
-    @Override
-    public void onPause()
-    {
-        super.onPause();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        if (!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
-        } else {
-            Log.d(TAG, "OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
-    public void onDestroy() {
-        super.onDestroy();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
+    @Override
+    public void onCameraViewStarted(final int width,
+                                    final int height) {
     }
 
-    public void onCameraViewStarted(int width, int height) {
-    }
-
+    @Override
     public void onCameraViewStopped() {
     }
 
-    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-        return inputFrame.rgba();
+    @Override
+    public Mat onCameraFrame(final CvCameraViewFrame inputFrame) {
+        final Mat rgba = inputFrame.rgba();
+
+        if (mIsPhotoPending) {
+            mIsPhotoPending = false;
+            takePhoto(rgba);
+        }
+
+        if (mIsCameraFrontFacing) {
+            // Mirror (horizontally flip) the preview.
+            Core.flip(rgba, rgba, 1);
+        }
+
+        return rgba;
+    }
+
+    private void takePhoto(final Mat rgba) {
+
+        // Determine the path and metadata for the photo.
+        final long currentTimeMillis = System.currentTimeMillis();
+        final String appName = getString(R.string.app_name);
+        final String galleryPath =
+                Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_PICTURES).toString();
+        final String albumPath = galleryPath + File.separator +
+                appName;
+        final String photoPath = albumPath + File.separator +
+                currentTimeMillis + Image_activity.PHOTO_FILE_EXTENSION;
+        final ContentValues values = new ContentValues();
+        values.put(MediaStore.MediaColumns.DATA, photoPath);
+        values.put(Images.Media.MIME_TYPE,
+                Image_activity.PHOTO_MIME_TYPE);
+        values.put(Images.Media.TITLE, appName);
+        values.put(Images.Media.DESCRIPTION, appName);
+        values.put(Images.Media.DATE_TAKEN, currentTimeMillis);
+
+        // Ensure that the album directory exists.
+        File album = new File(albumPath);
+        if (!album.isDirectory() && !album.mkdirs()) {
+            Log.e(TAG, "Failed to create album directory at " +
+                    albumPath);
+            onTakePhotoFailed();
+            return;
+        }
+
+        // Try to create the photo.
+        Imgproc.cvtColor(rgba, mBgr, Imgproc.COLOR_RGBA2BGR, 3);
+        if (!Imgcodecs.imwrite(photoPath, mBgr)) {
+            Log.e(TAG, "Failed to save photo to " + photoPath);
+            onTakePhotoFailed();
+        }
+        Log.d(TAG, "Photo saved successfully to " + photoPath);
+
+        // Try to insert the photo into the MediaStore.
+        Uri uri;
+        try {
+            uri = getContentResolver().insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        } catch (final Exception e) {
+            Log.e(TAG, "Failed to insert photo into MediaStore");
+            e.printStackTrace();
+
+            // Since the insertion failed, delete the photo.
+            File photo = new File(photoPath);
+            if (!photo.delete()) {
+                Log.e(TAG, "Failed to delete non-inserted photo");
+            }
+
+            onTakePhotoFailed();
+            return;
+        }
+
+        // Open the photo in LabActivity.
+        final Intent intent = new Intent(this, Image_activity.class);
+        intent.putExtra(Image_activity.EXTRA_PHOTO_URI, uri);
+        intent.putExtra(Image_activity.EXTRA_PHOTO_DATA_PATH,
+                photoPath);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void onTakePhotoFailed() {
+        mIsMenuLocked = false;
+
+        // Show an error message.
+        final String errorMessage =
+                getString(R.string.photo_error_message);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, errorMessage,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
